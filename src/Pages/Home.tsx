c666cd7121c439abb9a816b3c3ad0f4df2d7cc69
Swarna7414 <swarnasaisankar044@gmail.com>
2025-10-03 14:05:47 -0500

    added bg image

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import bgimage from "../assets/BackLogo.png";
+// import anotherbgimage from "../assets/logo.svg";
 
 const Home: React.FC = () => {
   return (
     <div className="pt-20 min-h-screen bg-gray-50">
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-xl text-gray-800 mb-8">Home</h1>
+      <div className="container mx-auto px-4 py-8 flex items-center justify-center">
+        <img src={bgimage} alt="bgimage" />
+        {/* <img src={anotherbgimage} alt="bgimage" /> */}
       </div>
     </div>
   );
